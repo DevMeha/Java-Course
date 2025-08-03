d040dc0c69be4a2e875a6a9a2d70946c110642eb
DevMeha <mixiadami@gmail.com>
2025-08-03 23:15:42 +0200

    First Commit Java Course
    Signed-off-by: DevMeha <mixiadami@gmail.com>

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main{
+    public static void main(String[] args) {
+       int mySecoudNumber = 12;
+       int myThirdNumber = 70;
+       int myFirstNumber = 35;
+       int myTotal = 117;
+       int myLastOne = 883;
+
+       int All = myFirstNumber + myThirdNumber + myFirstNumber + mySecoudNumber + myThirdNumber + myTotal + myLastOne;
+System.out.println(All);
+    }
+}
+
+
+
+
