ecdc6cb66e488ce66b079bf90e3fd42d63749cf4
DevMeha <mixiadami@gmail.com>
2025-08-04 23:37:31 +0200

    Learned about variables: int, byte, short, and long; and how to print their value ranges
    Signed-off-by: DevMeha <mixiadami@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,18 @@ public class Main{
 
        int All = myFirstNumber + myThirdNumber + myFirstNumber + mySecoudNumber + myThirdNumber + myTotal + myLastOne;
 System.out.println(All);
+
+
+int myValue = 10000;
+        int myMinValue = Integer.MIN_VALUE;
+int  myMaxValue = Integer.MAX_VALUE;
+System.out.println("Integer Value Range(" +  myMinValue + " to "  +   myMaxValue + ")");
+System.out.println("Byte Value Range(" +  Byte.MIN_VALUE + " to "  +   Byte.MAX_VALUE + ")");
+System.out.println("Short Value Range (" + Short.MIN_VALUE + " to " + Short.MAX_VALUE + ")");
+System.out.println("Long Value Range ("+ Long.MIN_VALUE + " to " + Long.MAX_VALUE+ ")");
+long myLong = 100L;
+System.out.println("A Long a width of" + Long.SIZE );
+
     }
 }
 
