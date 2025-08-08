9f8bcc703b74d13d9fe957a53afd5f2f1d9a36f5
DevMeha <mixiadami@gmail.com>
2025-08-08 11:13:27 +0200

    IF/ELSE/ELSE IF
    Signed-off-by: DevMeha <mixiadami@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -145,6 +145,26 @@ public class Main {
 
         System.out.println(resultAge);
 
+
+        //     ==============================> IF ELSE  <===============================
+
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 bute greater than 1000");
+        } else if (score < 1000) {
+
+            System.out.println("Your score was less than 1000");
+
+        } else {
+            System.out.println("Got here");
+        }
+
+
     }
 }
 
