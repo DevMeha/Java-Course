b17829524d203af46f514b38802202dd978924e4
DevMeha <mixiadami@gmail.com>
2025-08-10 09:06:39 +0200

    Create CHALLENGE
    Signed-off-by: DevMeha <mixiadami@gmail.com>

---
+++ src/Chall/Chall_v3.java
@@ -0,0 +1,19 @@
+package Chall;
+
+public class Chall_v3 {
+
+    public static void displayHighScorePositon(String namePlayer, int playerPosition) {
+
+        String name = Player1;
+
+        System.out.println("Tim managed to get into position on the high score list");
+
+    }
+
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+
+        return playerScore;
+    }
+}
