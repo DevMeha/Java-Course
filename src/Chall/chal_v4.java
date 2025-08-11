342c5f29be45d76cd1a8d0301179e220abf2af2c
DevMeha <mixiadami@gmail.com>
2025-08-11 22:12:53 +0200

    CHALLENGE USE match.round
    Signed-off-by: DevMeha <mixiadami@gmail.com>

---
+++ src/Chall/chal_v4.java
@@ -0,0 +1,56 @@
+package Chall;
+
+public class chal_v4 {
+
+    public static class SpeedConverter {
+
+        public static long toMilesPerHour(double kilometersPerHour) {
+            if (kilometersPerHour < 0) {
+                return -1;
+            } else {
+
+                return Math.round(kilometersPerHour * 0.621371);
+
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(SpeedConverter.toMilesPerHour(1.5));
+        System.out.println(SpeedConverter.toMilesPerHour(10.25));
+        System.out.println(SpeedConverter.toMilesPerHour(-5.6));
+        System.out.println(SpeedConverter.toMilesPerHour(25.42));
+        System.out.println(SpeedConverter.toMilesPerHour(75.114));
+
+
+    }
+
+}
+
+
+//Task
+
+
+// 1. Napisz metodę o nazwie toMilesPerHour, która przyjmuje 1 parametr typu double o nazwie kilometersPerHour.
+// Metoda ta powinna zwrócić zaokrągloną wartość wyniku obliczenia typu long.
+
+// Jeśli parametr kilometersPerHour jest mniejszy niż 0, metoda toMilesPerHour powinna zwrócić -1,
+// aby wskazać nieprawidłową wartość.
+
+// W przeciwnym razie, jeśli jest to wartość dodatnia, należy obliczyć wartość prędkości w milach na godzinę,
+// zaokrąglić ją i zwrócić wynik. Użyj w tym celu metody Math.round().
+
+
+// 2. Napisz metodę o nazwie printConversion, która przyjmuje 1 parametr typu double o nazwie kilometersPerHour.
+
+// Metoda ta nie powinna nic zwracać (typ void) i powinna obliczyć prędkość w milach na godzinę z parametru kilometersPerHour.
+
+// Następnie powinna wydrukować komunikat w formacie "XX km/h = YY mi/h".
+
+// XX to pierwotna wartość kilometersPerHour, a YY to zaokrąglona wartość prędkości w milach na godzinę uzyskana z parametru kilometersPerHour.
+
+// Jeśli parametr kilometersPerHour jest mniejszy niż 0, metoda powinna wydrukować tekst "Invalid Value".
