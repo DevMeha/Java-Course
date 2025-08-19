1b2c767b2d975d56ec8c19037140e736ce339e6c
DevMeha <mixiadami@gmail.com>
2025-08-19 22:26:36 +0200

    Switch  Expression
    Signed-off-by: DevMeha <mixiadami@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -217,33 +217,13 @@ public class Main {
     public static String getQuarter(String month) {
 
 
-        switch (month) {
-
-            case "January":
-            case "February":
-            case "March":
-                return "1st";
-
-
-            case "April":
-            case "May":
-            case "June":
-                return "2nd";
-
-
-            case "July":
-            case "August":
-            case "September":
-                return "3rd";
-
-            case "October":
-            case "November":
-            case "December":
-                return "4th";
-
-            default:
-                return "bad";
-        }
+        return switch (month) {
+            case "January", "February", "March" -> "1st";
+            case "April", "May", "June" -> "2nd";
+            case "July", "August", "September" -> "3rd";
+            case "October", "November", "December" -> "4th";
+            default -> "bad";
+        };
 
 
     }
