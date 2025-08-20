e9bbcd5c507a56e32611c25d77d3360614afeeeb
DevMeha <mixiadami@gmail.com>
2025-08-20 22:41:38 +0200

    int to string use switch and if and print this in method main
    Signed-off-by: DevMeha <mixiadami@gmail.com>

--- src/Chall/Chal_14.java
+++ src/Chall/Chal_14.java
@@ -5,20 +5,20 @@ public class Chal_14 {
     public static void printDayOfWeek(int day) {
 
 
-        switch (day) {
+        String dayOfWeek = switch (day) {
 
 
-            case 0 -> System.out.println("Sunday");
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            default -> System.out.println("Invalid Value");
-        }
-        ;
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Value";
+        };
 
+        System.out.println("Number " + day + " is " + dayOfWeek);
 
     }
 
@@ -31,7 +31,7 @@ public class Chal_14 {
         printDayOfWeek(5);
         printDayOfWeek(6);
         printDayOfWeek(324);
-        printWeekDay(332);
+
     }
 
 
