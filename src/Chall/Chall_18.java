cd885eacaaa53a46be09a42a2955780b374c85b0
DevMeha <mixiadami@gmail.com>
2025-08-26 01:03:26 +0200

    //PROGRAM KROTY DODAJE TYLKO PARZYSTE LICZBY Z PODANEJ LICZBY
    Signed-off-by: DevMeha <mixiadami@gmail.com>

---
+++ src/Chall/Chall_18.java
@@ -0,0 +1,45 @@
+package Chall;
+
+public class Chall_18 {
+
+    public static void main(String[] args) {
+
+        System.out.println(getEvenDigitSum(23343));
+
+    }
+
+    public static int getEvenDigitSum(int number) {
+
+        if (number < 0) {
+
+            return -1;
+
+
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+
+            int digit = number % 10;
+
+            if (digit % 2 == 0) {
+
+                sum += digit;
+
+            }
+
+            number = number / 10;
+
+
+        }
+
+
+        return sum;
+    }
+
+
+}
+
+
+//PROGRAM KROTY DODAJE TYLKO PARZYSTE LICZBY Z PODANEJ LICZBY
